2e4b090ae2d677c6afcfb472f5e0e2288a220d00
kaihsin <kaihsinwu@gmail.com>
2024-12-03 04:06:25 -0500

    bump pre-commit version

--- test/test_simple.py
+++ test/test_simple.py
@@ -1,11 +1,13 @@
-from cytnx_core import hello, MyClass
+from cytnx_core import MyClass, hello
 
 
 def test_str():
     assert isinstance(hello(), str)
 
+
 def test_myclass():
-    x=MyClass(num=3)
+    x = MyClass(num=3)
     assert x.get_num() == 3
+
 
 print(hello())
